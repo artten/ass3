7514ba7a364df0f188986ec1f41c9a6b72118ac1
Artiom Divak <artten@gmail.com>
2021-04-20 15:25:40 +0300

    GamEnvironment and CollisionInfo added

--- src/CollisionInfo.java
+++ src/CollisionInfo.java
@@ -1,11 +1,34 @@
+/**
+ * @author 319339198
+ * saves the colition info
+ */
 public class CollisionInfo {
-    // the point at which the collision occurs.
-    public Point collisionPoint() {
-        return null;
+    private Point collision;
+    private Collidable collisionObject;
+
+    /**
+     * constructor.
+     * @param collision - collision point
+     * @param collisionObject - the object to collide to
+     */
+    public CollisionInfo (Point collision, Collidable collisionObject){
+        this.collisionObject = collisionObject;
+        this.collision = collision;
     }
 
-    // the collidable object involved in the collision.
+    /**
+     * return the collision point
+     * @return the collision point
+     */
+    public Point collisionPoint() {
+        return collision;
+    }
+
+    /**
+     * return the collidable object involved in the collision.
+     * @return the collidable object involved in the collision.
+     */
     public Collidable collisionObject() {
-        return null;
+        return collisionObject;
     }
 }
--- src/GameEnvironment.java
+++ src/GameEnvironment.java
@@ -1,16 +1,56 @@
+/**
+ * @author 319339198
+ */
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * creates a new game
+ */
 public class GameEnvironment {
+    private List<Collidable> collidables;
 
-    // add the given collidable to the environment.
-    public void addCollidable(Collidable c) {
-
+    /**
+     * constructor.
+     */
+    public GameEnvironment() {
+        this.collidables = new LinkedList<>();
     }
 
-    // Assume an object moving from line.start() to line.end().
-    // If this object will not collide with any of the collidables
-    // in this collection, return null. Else, return the information
-    // about the closest collision that is going to occur.
+    /**
+     * add the given collidable to the environment.
+     * @param c - new collidable
+     */
+    public void addCollidable(Collidable c) {
+        this.collidables.add(c);
+    }
+
+    /**
+     * Assume an object moving from line.start() to line.end().
+     * If this object will not collide with any of the collidables
+     * in this collection, return null. Else, return the information
+     * about the closest collision that is going to occur.
+     * @param trajectory - the line of the ball move
+     * @return the closest collision
+     */
     public CollisionInfo getClosestCollision(Line trajectory) {
-        return null;
+        CollisionInfo collisionInfo = null;
+        for (Collidable collidable : this.collidables) {
+            if(collidable instanceof Block) {
+                Rectangle block = collidable.getCollisionRectangle();
+                Point closestCollision = trajectory.closestIntersectionToStartOfLine(block);
+                if(collisionInfo == null) {
+                    collisionInfo = new CollisionInfo(closestCollision,collidable);
+                }
+                else {
+                    if (trajectory.start().distance(closestCollision)
+                            < trajectory.start().distance(collisionInfo.collisionPoint())){
+                        collisionInfo = new CollisionInfo(closestCollision,collidable);
+                    }
+                }
+            }
+        }
+        return collisionInfo;
     }
 
 }
